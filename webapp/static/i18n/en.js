691d2fcb65d788ef00fe0803b243429d4fd4916f
sonia <sonia@kubusmedia.com>
2021-02-03 00:46:12 +0100

    First version of landing page!!! :rocket:

--- webapp/static/i18n/en.js
+++ webapp/static/i18n/en.js
@@ -1,3 +1,4 @@
 const en = {
-    home_pitch: 'Celebrate your special moments with the people you love the most, wherever they are.'
+    home_pitch: 'Celebrate your special moments with the people you love the most, wherever they are.',
+    home_sub_pitch: 'Get early access, get notified when the product is ready and get a FREE lifetime licence.'
 };
